90b293e9fc88c8905b83a3ac10546dd7e0389a57
Prabhu1403 <prabhubsccs24@gmail.com>
2026-09-12 16:20:38 +0530

    Use DATABASE_URL with SSL and explicit pg module

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,22 +4,41 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME!,
-    process.env.DB_USER!,
-    process.env.DB_PASSWORD!,
-    {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME!,
+//     process.env.DB_USER!,
+//     process.env.DB_PASSWORD!,
+//     {
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT),
+//     dialect: "postgres",
+//     logging:false,
+//     pool: {
+//             max: 5,
+//             min: 0,
+//             acquire: 30000,
+//             idle: 10000
+//         }
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL!, {
     dialect: "postgres",
-    logging:false,
+    dialectModule: pg,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+    logging: false,
     pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 });
+
 
 async function connect() {
     try {
